18068db5414ce7fc2515d5af6d4cfb01151616d9
hvklkn <hvklkn@icloud.com>
2022-12-11 19:36:36 +0300

    tezler (product) oluşturup migrate ettim

--- bitirmetezi/bitirmetezi/urls.py
+++ bitirmetezi/bitirmetezi/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls'))
+    
+    path('tezler/',include('tezler.urls')),
+    path('',include('home.urls')),
 ]
