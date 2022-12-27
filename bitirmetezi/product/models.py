74e5ad5cf9272541f008b7737817b5fc75a22646
hvklkn <hvklkn@icloud.com>
2022-12-27 13:44:14 +0300

    sjvh

--- bitirmetezi/product/models.py
+++ bitirmetezi/product/models.py
@@ -47,4 +47,7 @@ class Images(models.Model):
     title = models.CharField(max_length = 50)
     image = models.ImageField(blank=True,upload_to= 'images/')
 
+def __str__(self):
+        return self.title
+
         
